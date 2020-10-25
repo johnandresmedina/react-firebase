0948106038aff58fd56b6fa8914a1ad991cd71c1
John Medina <jhonandresmedina@gmail.com>
2020-10-25 13:45:31 -0500

    Moving auth observer to main component

--- src/index.js
+++ src/index.js
@@ -6,23 +6,17 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import * as serviceWorker from './serviceWorker';
-import firebase from './config/firebase';
-import AuthProvider from './context/authContext';
 import Main from './main';
 import theme from './theme';
 
-firebase.auth().onAuthStateChanged(user => {
-  render(
-    <AuthProvider user={user}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Main />
-      </ThemeProvider>
-    </AuthProvider>,
+render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Main />
+  </ThemeProvider>,
 
-    document.getElementById('root'),
-  );
-});
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import AuthProvider from '../context/authContext';
+import firebase from '../config/firebase';
 import ProjectProvider from '../context/projectContext';
 import Routes from './Routes';
 
 const Main = () => {
+  const [user, setUser] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(false);
+      }
+      setLoaded(true);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <ProjectProvider>
-      <Routes />
-    </ProjectProvider>
+    <>
+      {loaded ? (
+        <AuthProvider user={user}>
+          <ProjectProvider>
+            <Routes />
+          </ProjectProvider>
+        </AuthProvider>
+      ) : null}
+    </>
   );
 };
 
