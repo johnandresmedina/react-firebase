57649a7fcf880b9fad928a7ab047a3b3fe69ace1
John Medina <jhonandresmedina@gmail.com>
2020-11-02 18:10:13 -0500

    Adding notifications

--- src/dashboard/Notifications.js
+++ src/dashboard/Notifications.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
+import { Box, Paper, Typography, List, ListItem, ListItemText } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  list: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+}));
 
 export default function Notifications() {
+  const classes = useStyles();
+
   return (
-    <Box my={2} paddingX={3}>
-      Notifications
+    <Box className='project-summary' component={Paper} elevation={2} square mb={1} my={2} paddingX={3}>
+      <Box className='card-content' p={2}>
+        <Box className='card-title'>
+          <Typography variant='h5'>Notifications</Typography>
+        </Box>
+        <List dense={false}>
+          <ListItem className={classes.list}>
+            <ListItemText primary='Notification 1' />
+            <ListItemText primary='Notification 2' />
+            <ListItemText primary='Notification 3' />
+            <ListItemText primary='Notification 4' />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 }
